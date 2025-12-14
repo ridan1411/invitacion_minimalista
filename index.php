c79b2de4e88dbac473857735133c1c434c7e658e
Carlos Ridan <ridan.1411@gmail.com>
2025-12-14 13:34:47 -0600

    cambio fecha counter

--- index.php
+++ index.php
@@ -728,10 +728,10 @@
     <script src="js/magnific-popup-options.js"></script>
 
     <!-- // <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js"></script> -->
-    <script src="js/simplyCountdown.min.js"></script>
-    <script src="js/simplyCountdown.js"></script>
+    <script src="js/simplyCountdown.min.js?v=5.1.1"></script>
+    <script src="js/simplyCountdown.js?v=5.1.1"></script>
     <!-- Main -->
-    <script src="js/main.js?v=4.0.0"></script>
+    <script src="js/main.js?v=5.1.1"></script>
 
     <!-- Modal Paroquia Img-->
     <div class="modal fade" id="parroquiaimg" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
