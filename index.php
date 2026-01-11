c54d302244ae0c729415873ca3dd17dd0842b977
pmxhomie <pmxhomie@hotmail.com>
2026-01-10 22:17:56 -0600

    Cambios en el header, se movio el boton Save the day y se regreso el form para la zona de confirmar asistencia

--- index.php
+++ index.php
@@ -159,8 +159,11 @@
                                         </audio>
                                     </div>
                                 </div>
-
-
+                                <br>
+                                                     <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                                                             <a target="_blank" href="https://calendar.app.google/gzY49r8J7HW3DaMp8" class="btn pb-2" style="background: #fdfdfd;
+                                                            border-color: #aa9274;">Save the Date</a>
+                                                    </div>
                             </div>
                         </div>
                     </div>
@@ -650,46 +653,40 @@
     </div>
 
     <!-- Aviso -->
-    <div id="fh5co-started" class="fh5co-bg" style="background-image:url(images/fondo_seda.jpg);">
+     <div id="fh5co-started" class="fh5co-bg" style="background-image:url(images/fondo_seda.jpg);">
         <div class="overlay" style="background: rgb(0 0 0 / 76%);"></div>
         <div id="asistencia" class="container">
             <div class="row animate-box">
                 <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
 
-                    <!-- <p style="color: white; margin-top: 20px;">Estamos emocionados de celebrar nuestro día especial y nos encantaría contar con su presencia.</p> -->
-                    <p><strong>
                             <h4 style="color:white; font-family: BenchNine, sans-serif; !important"">Su presencia será el regalo más grande. Confirma pronto para hacer de este día un momento inolvidable.</h4>
-                        </strong></p>
-                </div>
 
-                <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
-                        <a target="_blank" href="https://calendar.app.google/gzY49r8J7HW3DaMp8" class="btn " style="background: #fdfdfd;
-                        border-color: #aa9274; height: 40px;">Save the Date</a>
-                </div>
-
-            </div>
-            <div class="row animate-box" style="margin-bottom: 20px;">
-                <div class="col-md-10 col-md-offset-1 text-center">
-                    <!-- <form class="form-inline">
-                        <div class="col-md-8 col-sm-8">
-                            <div id="inpName" class="form-group">
-                                <label for="name" class="sr-only">Nombre</label>
-                                <input style="font-family: Oswald, sans-serif;" name="name" type="name" class="form-control" id="name" placeholder="Escribe tu nombre aquí y el de tus acompañantes.">
-                            </div>
-                        </div> -->
-                        <!--div class="col-md-3 col-sm-3">
-                            <div id="inpPers" class="form-group">
-                                <label for="pers" class="sr-only">Personas</label>
-                                <input name="pers" type="number" class="form-control" id="pers"
-                                    placeholder="N° Personas">
-                            </div>
-                        </div-->
-                        <!-- <div class="col-md-4 col-sm-4">
-                            <a style="padding-top: 16px; padding-left: 10px; padding-right: 10px; margin-top: 0px; font-size: 14px;" id="btnconf" type="submit" class="btn btn-default btn-block">Confirmar Asistencia</a>
                         </div>
-                    </form> -->
                 </div>
-            </div>
+                <div class="row animate-box" style="margin-bottom: 20px;">
+                        <div class="col-md-10 col-md-offset-1 text-center">
+                                <form class="form-inline">
+                                        <div class="col-md-8 col-sm-8">
+                                            <div id="inpName" class="form-group">
+                                                <label for="name" class="sr-only">Nombre</label>
+                                                <input name="name" type="name" class="form-control" id="name"
+                                                    placeholder="Escribe tu nombre aquí y el de tus acompañantes.">
+                                            </div>
+                                        </div>
+                                    <!--div class="col-md-3 col-sm-3">
+                                        <div id="inpPers" class="form-group">
+                                            <label for="pers" class="sr-only">Personas</label>
+                                            <input name="pers" type="number" class="form-control" id="pers"
+                                                placeholder="N° Personas">
+                                        </div>
+                                    </div-->
+                                        <div class="col-md-4 col-sm-4">
+                                            <a style="padding-top: 16px; padding-left: 10px; padding-right: 10px; margin-top: 0px; font-size: 14px;" id="btnconf" type="submit"
+                                                class="btn btn-default btn-block">Confirmar Asistencia</a>
+                                        </div>
+                                </form>
+                        </div>
+                </div>
         </div>
     </div>
 
